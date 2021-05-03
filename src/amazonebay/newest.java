4769dc205c9270226eb95e3e8c83ab0bb15b2ff3
sheriff1129 <doublebb98@yahoo.com>
2021-05-03 19:10:32 +0000

    Commit message

---
+++ src/amazonebay/newest.java
@@ -0,0 +1,16 @@
+package amazonebay;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class newest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\sheri\\Downloads\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.ebay.co.uk/");
+		
+
+	}
+
+}
